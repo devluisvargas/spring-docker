b06b09a369021d09e11828a81181e701e8509577
lbvargas <luisbyronvargaspenafiel@gmail.com>
2023-11-10 22:36:50 -0500

    inicio ms usuario

---
+++ ms-comp-usuasrio/src/main/java/com/devluis/mscompusuasrio/MsCompUsuasrioApplication.java
@@ -0,0 +1,13 @@
+package com.devluis.mscompusuasrio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCompUsuasrioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsCompUsuasrioApplication.class, args);
+    }
+
+}
---
+++ ms-comp-usuasrio/src/test/java/com/devluis/mscompusuasrio/MsCompUsuasrioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devluis.mscompusuasrio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCompUsuasrioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
