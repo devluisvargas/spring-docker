ba152420ade47b7f6a0c7fce71fb048410dcd5c3
lbvargas <luisbyronvargaspenafiel@gmail.com>
2023-11-13 22:41:15 -0500

    creacion de ms-comp-gateway y configuracion

---
+++ ms-comp-gateway/src/main/java/com/devluis/mscompgateway/MsCompGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.devluis.mscompgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCompGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsCompGatewayApplication.class, args);
+    }
+
+}
---
+++ ms-comp-gateway/src/test/java/com/devluis/mscompgateway/MsCompGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devluis.mscompgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCompGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
